57080eebc21ad2c847e271cefc22772feb1ec6ec
ken-genesius <mkengenesius@gmail.com>
2025-03-26 03:45:34 +0700

    deploy(approval-feature): Deploy Approval Feature to Production

---
+++ Migrations/20250325204117_approval-note.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrewMate.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class approvalnote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApprovalNote",
+                table: "LeaveApplications",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovalNote",
+                table: "LeaveApplications");
+        }
+    }
+}
